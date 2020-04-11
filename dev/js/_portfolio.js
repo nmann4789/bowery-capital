99e674cb55c89f7358916245d44eddd470e2b293
Nicholas Mann <nmann@C02XD0H0JGH5.local>
2020-04-11 15:41:35 -0400

    fix click handler

--- dev/js/_portfolio.js
+++ dev/js/_portfolio.js
@@ -3,23 +3,14 @@ const _portfolio = {
   settings: {
 
   },
-  addPopoutHandler: function(popout){
-    console.log('adding click handler')
-    popout.on('click', function(){
-      console.log('opening popout')
-      $('.card__portfolio--popout').removeClass('active')
-      $(this).find('.card__portfolio--popout').addClass('active')
-      $(this).off()
-    })
-  },
   controlPortolioPopouts: function(){
     $('.card__portfolio--popout--close').on('click', function(){
-      console.log('closing popout')
       $(this).closest('.card__portfolio--popout').removeClass('active')
-      _portfolio.addPopoutHandler($(this).closest('.card__portfolio'))
     })
-    $('.card__portfolio').each(function(){
-      _portfolio.addPopoutHandler($(this))
+    $('.card__portfolio--preview').on('click', function(){
+      $('.card__portfolio--popout').removeClass('active')
+      $(this).closest('.card__portfolio').find('.card__portfolio--popout').addClass('active')
+
     })
   },
   init: function () {
